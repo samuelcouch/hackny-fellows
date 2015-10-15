aa52d7d86669206219e9df075a8c2a826100cd07
Sam Couch <samcouch91@gmail.com>
2015-10-14 21:56:32 -0400

    general revisions, added example ruby file

---
+++ 2016/example.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'ostruct'
+
+class Demo
+  def initialize(data)
+    data_to_accessors(data)
+  end
+
+  private
+  ##
+  # Map the data to attr_accessor's
+  def data_to_accessors (data)
+    data_os = OpenStruct.new(data)
+    data_os.each_pair do |var, val|
+      (class << self ; self ; end).class_eval { attr_accessor var }
+      instance_variable_set "@#{var}", val
+    end
+  end
+end
+
+test_data = { :sound_level => 11, :status => "rockin" }
+test = Demo.new(test_data)
+puts test.status # => "rockin"
+puts test.sound_level # => 11
+# now lets change one of the values
+test.status = "still rockin"
+puts test.status # => "still rockin"
